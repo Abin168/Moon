4a3df65977e6bf543f4b2dae48a0c118715e73ba
Poseidon <test@gamil.com>
2023-12-15 16:11:18 +0800

    moon-business-rating: fix read config issue when config is application.yml

---
+++ moon-business-rating/src/main/java/com/moon/business/rating/MoonBusinessRating.java
@@ -0,0 +1,14 @@
+package com.moon.business.rating;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MoonBusinessRating {
+    public static void main(String[] args) {
+        SpringApplication.run(MoonBusinessRating.class, args);
+    }
+}
