314cf2cf57ef687e41bf224d4cb69ba4508da2a5
poseidon <jack.robert05263@gmail.com>
2024-01-08 15:33:39 +0800

    moon-business-rating: mybatis @MapperScan

--- moon-business-rating/src/main/java/com/moon/business/rating/MoonBusinessRating.java
+++ moon-business-rating/src/main/java/com/moon/business/rating/MoonBusinessRating.java
@@ -1,11 +1,13 @@
 package com.moon.business.rating;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan({"com.moon.business.rating.mapper"})
 public class MoonBusinessRating {
     public static void main(String[] args) {
         SpringApplication.run(MoonBusinessRating.class, args);
